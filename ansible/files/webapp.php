30b7b45b7783ee5ab01faac82d8a48f2003c212c
Alucardesu <oscar_roman@epam.com>
2022-05-27 14:56:00 -0600

    Adding basic app

---
+++ ansible/files/webapp.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="/insert.php">
+  <label for="iname">Item Name: </label>
+  <input type="text" id="iname" name="iname" value="" maxlength="20">
+  <label for="number">Quantity: </label>
+  <input type="number" id="quantity" name="quantity" min="1" max="5">
+  <input type="submit" value="Submit">
+</form> 
+<br>
+<form method="get" action="/select.php">
+  <button type="submit">Select</button>
+</form>
+
+</body>
+</html>
